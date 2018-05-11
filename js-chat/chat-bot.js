8b998a814ddd9aa28f8acb6d3e84db62894436f8
Unknown <inge_andres2811@hotmail.com>
2018-05-10 22:17:53 -0500

    tome y no joda!!

--- js-chat/chat-bot.js
+++ js-chat/chat-bot.js
@@ -21,39 +21,35 @@ function chatBot() {
 			return "Hola player [^_^] ..!!!";
 
 		if(this.match('(como estas?|como estas|que tal|como va todo|que onda|que honda)'))
-			return ["todo esta excelente [^_^]", "en que podria ayudarte? tengo las siguientes opciones", "(1)reglas del juego","(2)contacto de mis padres","(3)informacion a cerca del juego","(3)quienes somos","(4)si estas aburrido te puedo dar un acertijo [^_^] "];
+			return ["todo esta excelente [^_^]", "en que podria ayudarte?", "(1)reglas del juego","(2)informacion sobre nosotros","(3)si estas aburrido te puedo dar un acertijo [^_^] "];
 
-		if(this.match('(reglas|me podrias decir las reglas?|cuales son las reglas|reglas por favor|reglas)'))
-			return ["las reglas son blablablbalblbalbalblablablalbalbalbalblablalbalblalballbalbalalba [^_^]"];
-		
-		if(this.match('(quien eres?|quien eres|que eres|que es|quien escribe?|quien escribe|eres humano?|eres un robot?|eres un fantasma?)'))
-			return ["soy un robot artificial el cual desea colaborar, para esto me han desarrollado mis padres [^_^]", "en que podria ayudarte?"];
         
-        if(this.match('(que ofreces?|que opciones tienes|opciones|probabilidades|que me puedes dar?)'))
-			return ["te puedo decir: ", "reglas del juego","contacto de mis padres","informacion a cerca del juego","quienes somos","o si estas aburrido te puedo dar un acertijo [^_^] "];
-		
 		if(this.match('(1)'))
-			return ["las reglas son blablablbalblbalbalblablablalbalbalbalblablalbalblalballbalbalalba ","te puedo ayudar en algo mas? [^_^]"];
+			return ["las reglas de MENTAL WAR son simples","no puedes caer porque la lava te matara","debes escalar rapidamente para evitar que las bombas hagan PUMMMM","entre mas puntos obtengas seras un mejor ganador","cada nivel es mas dificil, ten cuidado ","te podria ayudar en algo mas? [^_^]"];
 		
 		if(this.match('(2)'))
-			return ["el contacto de mis padres es  ","te puedo ayudar en algo mas? [^_^]"];
+			return ["mis padres son estudiantes de ingenieria de sistemas los cuales estan adquieriendo conocimientos para darme vida ","este mundo en el que estas sumergido es creado por ellos para tu diversion por supuesto","emmmm y yooo ummmm..."];
 		
 		if(this.match('(3)'))
-			return ["el juego es bla ","te puedo ayudar en algo mas? [^_^]"];
-		if(this.match('(4)'))
-			return [" somos bla ","te puedo ayudar en algo mas? [^_^]"];
+			return ["cuantos animales caben en una vallena?","(a)uffffff muchos!!!","(b)ninguno porque va llena :V","(c)jijuemil","(d) no se :o"];
 
-		if(this.match('(5)'))
-			return [" tu acertijo es: ","te puedo ayudar en algo mas? [^_^]"];
+		if(this.match('(a|b|c|d)'))
+			return [" respondiste mal... o bien?? [^-^] no se pero bueno :v","te puedo colaborar en algo mas? [^_^]"];
 
 		if(this.match('^no+(\\s|!|\\.|$)'))
 			return "Espero no tengas mas inquietudes [^_^] ";
-		
-		if(this.match('(adios|chao|hasta luego|bye|vemos|hasta pronto)'))
-			return ["hasta pronto  [^_^]", "Mucha suerte!"];
+
+		if(this.match('(quien eres?|quien eres|que eres|que es|quien escribe?|quien escribe|eres humano?|eres un robot?|eres un fantasma?)'))
+			return ["soy un robot artificial el cual desea colaborar, para esto me han desarrollado mis padres [^_^]", "en que podria ayudarte?"];
 		
 		if(this.match('(hp|malparido|hijoputa|hijodeputa|gonorrea|pirobo)'))
 			return ["sinceramente no entiendo esta palabra ", "pero lo mismo para ti [^_^]"];
+
+
+		if(this.match('(adios|chao|hasta luego|bye|vemos|hasta pronto)'))
+			return ["hasta pronto  [^_^]", "Mucha suerte!"];
+
+		
 
 		if(this.match('(gracias)'))
 			return ["Con todo gusto!!!!"];
